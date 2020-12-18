aa36981774971aa33eed360b0f55c874088a0e98
Renuka42 <61011212062@msu.ac.th>
2020-12-18 16:33:06 +0700

    ttt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
-=======
 import { ProfileComponent } from './profile/profile.component';
 import { ListComponent } from './list/list.component';
 
->>>>>>> d7ab2645e71bd56ab6dfabbb906cc2e5aa341fcf
 @NgModule({
   declarations: [
     AppComponent,
